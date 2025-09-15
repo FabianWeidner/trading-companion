b9be0a7f6cfc9b715d90cc29a87cb3d23a3a6204
Fabian Phillipe <fabian.weidner@grapes-app.com>
2025-09-15 22:34:47 +0200

    Setup SQLAlchemy engine, SessionLocal and get_db dependency

--- backend/app/db/session.py
+++ backend/app/db/session.py
@@ -1,15 +1,16 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./dev.db"
+# SQLite Datenbank (lokal im Projektordner)
+SQLALCHEMY_DATABASE_URL = "sqlite:///./backend/app.db"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
 )
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 
-# Dependency für FastAPI
 def get_db():
     db = SessionLocal()
     try:
